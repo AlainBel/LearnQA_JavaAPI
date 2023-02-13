6574e75619dcdfefb0d2f508a1182bea2c38d598
alena.belousova <albelspb@gmail.com>
2023-02-14 01:25:48 +0400

    Add test for request check_type

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,29 +1,24 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class HelloWorldTest {
     @Test
     public void testRestAssured() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "John");
+        Map<String,Object> body = new HashMap<>();
+        body.put("param1","value1");
+        body.put("param2","value2");
 
-        JsonPath response = RestAssured
+        Response response = RestAssured
                 .given()
-                .queryParams(params)
-                .get("https://playground.learnqa.ru/api/hello")
-                .jsonPath();
+                .body(body)
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
 
-        String name = response.get("answer");
-        if (name == null) {
-            System.out.println("The key 'answer2' is absent");
-        } else {
-            System.out.println(name);
-        }
+        response.print();
     }
     @Test
     public void testGetText() {
