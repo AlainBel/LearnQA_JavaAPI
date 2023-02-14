67190894e93ee1545d5b42110a1f99754e9664b5
alena.belousova <albelspb@gmail.com>
2023-02-14 21:21:23 +0400

    Add test for status code

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,24 +1,17 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class HelloWorldTest {
     @Test
     public void testRestAssured() {
-        Map<String,Object> body = new HashMap<>();
-        body.put("param1","value1");
-        body.put("param2","value2");
 
         Response response = RestAssured
-                .given()
-                .body(body)
-                .post("https://playground.learnqa.ru/api/check_type")
+                .get("https://playground.learnqa.ru/api/get_500")
                 .andReturn();
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
 
-        response.print();
     }
     @Test
     public void testGetText() {
