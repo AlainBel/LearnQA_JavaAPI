6596eb611ca1f65c767af9f2ebaf03539bfb3149
alena.belousova <albelspb@gmail.com>
2023-02-15 22:32:54 +0400

    Add test for check_auth_cookie

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -13,23 +13,27 @@ public class HelloWorldTest {
         data.put ("login","secret_login");
         data.put("password","secret_pass");
 
-        Response response = RestAssured
+        Response responseForGet= RestAssured
                 .given()
                 .body (data)
                 .when()
-                .get("https://playground.learnqa.ru/api/get_auth_cookie")
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
                 .andReturn();
 
-        System.out.println("\nPretty text:");
-        response.prettyPrint();
+        String responseCookie = responseForGet.getCookie("auth_cookie");
 
-        System.out.println("\nHeaders:");
-        Headers responseHeaders = response.getHeaders();
-        System.out.println(responseHeaders);
+        Map<String,String> cookies = new HashMap<>();
+        cookies.put("auth_cookie", responseCookie);
 
-        System.out.println("\nCookies:");
-        Map<String,String> responseCookies = response.getCookies();
-        System.out.println(responseCookies);
+        Response responseForCheck = RestAssured
+                .given()
+                .body(data)
+                .cookies(cookies)
+                .when()
+                .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+        responseForCheck.print();
 
     }
     @Test
